90fb9cf4c1a143ac7d9a2a74956fc9d331f594d4
mari <35354893+shondofan2016@users.noreply.github.com>
2025-11-04 14:05:59 -0600

    Revert "Update MediaCarousel.tsx"
    This reverts commit 7bc18f9154ebafd783546f413b86955f68783447.

--- src/pages/discover/components/MediaCarousel.tsx
+++ src/pages/discover/components/MediaCarousel.tsx
@@ -540,7 +540,7 @@ export function MediaCarousel({
                       id: item.id.toString(),
                       title: item.title || item.name || "",
                       poster: item.poster_path
-                        ? `https://tmdb.pampaisd.org/site/aHR0cHM6Ly9pbWFnZS50bWRiLm9yZw==/t/p/w342${item.poster_path}`
+                        ? `https://image.tmdb.org/t/p/w342${item.poster_path}`
                         : "/placeholder.png",
                       type: isTVShow ? "show" : "movie",
                       year: isTVShow
