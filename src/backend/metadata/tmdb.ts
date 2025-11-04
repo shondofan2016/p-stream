b5fbaf31278700355042646098d5bcbdbf1e20f6
mari <35354893+shondofan2016@users.noreply.github.com>
2025-11-04 13:04:51 -0600

    Update tmdb.ts

--- src/backend/metadata/tmdb.ts
+++ src/backend/metadata/tmdb.ts
@@ -153,8 +153,8 @@ export function decodeTMDBId(
   };
 }
 
-const tmdbBaseUrl1 = "https://tmdb.pampaisd.org/site/https://api.themoviedb.org/3/";
-const tmdbBaseUrl2 = "https://tmdb.pampaisd.org/site/https://api.tmdb.org/3/";
+const tmdbBaseUrl1 = "https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudGhlbW92aWVkYi5vcmc=/";
+const tmdbBaseUrl2 = "https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudG1kYi5vcmc=/";
 
 const apiKey = conf().TMDB_READ_API_KEY;
 
