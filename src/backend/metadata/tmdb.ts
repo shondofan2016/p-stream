e3a46d1a786e88f0aad7f1efecd7b19b3e2fadb1
mari <35354893+shondofan2016@users.noreply.github.com>
2025-11-04 13:19:56 -0600

    Update tmdb.ts

--- src/backend/metadata/tmdb.ts
+++ src/backend/metadata/tmdb.ts
@@ -153,12 +153,8 @@ export function decodeTMDBId(
   };
 }
 
-const tmdbBaseUrl1 =
-  "https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudGhlbW92aWVkYi5vcmc=" +
-  "/";
-const tmdbBaseUrl2 =
-  "https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudG1kYi5vcmc=" +
-  "/";
+const tmdbBaseUrl1 = `https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudGhlbW92aWVkYi5vcmc=/`;
+const tmdbBaseUrl2 = `https://tmdb.pampaisd.org/site/aHR0cHM6Ly9hcGkudG1kYi5vcmc=/`;
 
 const apiKey = conf().TMDB_READ_API_KEY;
 
